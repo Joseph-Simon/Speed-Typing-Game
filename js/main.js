30b0a0d32007ef4c366b1cac32260c8d444869f8
Joseph-Simon <36781112+Joseph-Simon@users.noreply.github.com>
2018-08-20 19:27:24 -0400

    fixed local storage

--- js/main.js
+++ js/main.js
@@ -76,7 +76,7 @@ function getScore() {
     localStorage.setItem("highScore", 0);
   } else {
     highScore = localStorage.getItem("highScore");
-    console.log("Yep");
+    bestScore.innerHTML = localStorage.getItem("highScore");
   }
 }
 
